d20c521263860784eea390be28a588890a58c640
ellijs <ellijs@hotmail.com>
2021-12-17 03:40:31 -0500

    SearchBar

--- interview-prep2/src/App.js
+++ interview-prep2/src/App.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 function App() {
   const api = 'https://dog.ceo/api/breeds/list/all'
   const [ breeds, setBreeds ] = useState([])
+  const [ search, setSearch ] = useState('')
 
   useEffect(() => {
       fetch(api)
@@ -12,15 +13,22 @@ function App() {
       .then(data=>setBreeds(Object.keys(data.message)))
   }, [])
 
-  console.log(breeds)
+  console.log(search)
 
   const renderAllDogs = breeds.map(breed => <li key={breed}>{breed}</li>)
 
   return (
-    
-     <ul>
+    <>
+    <form>
+      <label>Enter Dog Breed</label>
+      <input onChange={(e)=>setSearch(e.target.value)} placeholder='Dog breed here...'></input>
+      <button>Search</button>
+    </form>
+
+     <ul className='dog-list'>
        {renderAllDogs}
      </ul>
+     </>
   );
 }
 
